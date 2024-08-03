75a7c09a508579dd4a481beb6554ae7b26860703
Ömer Faruk Kurt <ofk.kurt@hotmail.com>
2024-08-03 05:41:32 +0300

    55

---
+++ BusinessLayer/Concrete/HeadingManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class HeadingManager:IHeadingService
+    {
+        IHeadingDal _headingDal;
+
+        public HeadingManager(IHeadingDal headingDal)
+        {
+            _headingDal = headingDal;
+        }
+        public List<Heading> GetList()
+        {
+            return _headingDal.List();
+        }
+
+        public void HeadingAdd(Heading heading)
+        {
+            _headingDal.Insert(heading);
+        }
+
+        public Heading GetById(int id)
+        {
+            return _headingDal.Get(x => x.HeadingID == id);
+        }
+
+        public void HeadingDelete(Heading heading)
+        {
+            heading.HeadingStatus = false;
+            _headingDal.Update(heading);
+        }
+
+        public void HeadingUpdate(Heading heading)
+        {
+            _headingDal.Update(heading);
+        }
+    }
+}
---
+++ MVCProje/Controllers/HeadingController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+namespace MVCProje.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
+
+        public ActionResult Index()
+        {
+
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                select new SelectListItem
+                {
+                    Text = x.CategoryName,
+                    Value = x.CategoryID.ToString()
+                }).ToList();
+            List<SelectListItem> valuewriter = (from x in wm.GetList()
+                select new SelectListItem
+                {
+                    Text = x.WriterName + " " + x.WriterSurName,
+                    Value = x.WriterID.ToString()
+                }).ToList();
+            ViewBag.vlc = valuecategory;
+            ViewBag.vlc = valuewriter;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortTimeString());
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                select new SelectListItem
+                {
+                    Text = x.CategoryName,
+                    Value = x.CategoryID.ToString()
+                }).ToList();
+            ViewBag.vlc = valuecategory;
+            var headingvalues = hm.GetById(id);
+            return View(headingvalues);
+        }
+        
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.HeadingUpdate(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteHeading(int id)
+        {
+            var headingvalues = hm.GetById(id);
+            hm.HeadingDelete(headingvalues);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
